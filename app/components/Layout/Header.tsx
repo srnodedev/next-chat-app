13396a9ce4883259a7a8d03f501ccbc2584345ec
srnodedev <senior.node.dev@gmail.com>
2022-12-07 16:18:40 -0500

    adding nav items header

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -1,10 +1,24 @@
 import styles from './layout.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
+import Button from '../Button'
+import { links } from './links'
+
+interface LinkProps {
+    title ?: string,
+    link : string,
+}
 
 export default function Header() {
     return (
         <div className={styles.header}>
             <Image src='/icon.png' alt={'avatar'} width={27} height={21} />
+            <div>BrainBattle</div>
+            <div className={styles.push} />
+            {links.map((item : LinkProps, index) => 
+                <Link href={item.link} key={index} style={{marginLeft: 10}}> {item.title} </Link>
+            )}
+            {/* <Button>Sign in / Register</Button> */}
         </div>
     )
 }
---
+++ app/components/Layout/links.ts
@@ -0,0 +1,28 @@
+const links = [
+    {
+        title: 'Home',
+        link: '/',
+    },
+    {
+        title: 'Story',
+        link: '/',
+    },
+    {
+        title: 'Community',
+        link: '/',
+    },
+    {
+        title: 'Destinations',
+        link: '/',
+    },
+    {
+        title: 'Blog',
+        link: '/',
+    },
+    {
+        title: 'Help',
+        link: '/',
+    },
+];
+
+export {links};
