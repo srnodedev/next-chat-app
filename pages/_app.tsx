8b774b6e33b7ad7fed85bef97d47b0c099eca571
srnodedev <senior.node.dev@gmail.com>
2022-12-10 20:01:58 -0500

    change theme folder location

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'styled-components'
 import type { AppProps } from 'next/app'
-import {theme} from './theme'
+import {theme} from '../theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
---
+++ theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+	colors: {
+		accent: '#09142F',
+        accenthover: '#0E2846',
+        error: '#F13C3C',
+        warning: '#FFA826',
+        disabled: '#F4F6F9',
+        body: '#071B32',
+        secondary: '#9BA8BD',
+        white: '#FFFFFF',
+        gray1: '#E7E9F0',
+        grayforicons: '#F0F1F3'
+
+	},
+};
